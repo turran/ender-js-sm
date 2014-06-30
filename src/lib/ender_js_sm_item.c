ac52e4a3420ec4179e6d2753111a4d197d8d790a
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-06-30 20:14:39 +0200

    Add common code for items

---
+++ src/lib/ender_js_sm_item.c
@@ -0,0 +1,63 @@
+/* Ender Js SM - Ender bindings for JavaScript (SpiderMonkey based)
+ * Copyright (C) 2014 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ender_js_sm_private.h"
+#include "ender_js_sm_item_private.h"
+#include "ender_js_sm_object_private.h"
+#include "ender_js_sm_function_private.h"
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+void ender_js_sm_item_destroy(JSContext *cx, JSObject *obj)
+{
+	Ender_Item *i;
+
+	i = JS_GetPrivate(cx, obj);
+	ender_item_unref(i);
+}
+
+JSObject * ender_js_sm_item_create(JSContext *cx, Ender_Item *i)
+{
+	JSObject *ret = NULL;
+	Ender_Item_Type type;
+
+	type = ender_item_type_get(i);
+	switch (type)
+	{
+		case ENDER_ITEM_TYPE_FUNCTION:
+		ret = ender_js_sm_function_new(cx, i);
+		break;
+
+		case ENDER_ITEM_TYPE_OBJECT:
+		ret = ender_js_sm_object_new(cx, i);
+		break;
+
+		default:
+		ERR("Unsupported type '%s'", ender_item_type_name_get(type));
+		ender_item_unref(i);
+		break;
+	}
+	return ret;
+}
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+

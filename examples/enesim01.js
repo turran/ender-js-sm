b5ed34af1f4f7566edf95f10227fb41ca1596efe
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-07-02 00:41:51 +0200

    Do more experiments on the js example

--- examples/enesim01.js
+++ examples/enesim01.js
@@ -6,5 +6,8 @@ const enesim = ender.enesim;
 
 enesim.init();
 var r = new enesim.renderer.checker();
+var gn = r.getName;
+print(gn());
+print(r.getName());
 r.evenColor = 0xffffff;
 enesim.shutdown();

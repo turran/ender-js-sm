e51fae86fe6767c9a1ba993f6037651801a80880
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-07-02 20:51:49 +0200

    Make the example be the same as the neko one

--- examples/enesim01.js
+++ examples/enesim01.js
@@ -6,8 +6,19 @@ const enesim = ender.enesim;
 
 enesim.init();
 var r = new enesim.renderer.checker();
-var gn = r.getName;
-print(gn());
-print(r.getName());
-r.evenColor = 0xffffff;
+print("r = " + r.getName());
+
+r.evenColor = 0xff000000;
+r.oddColor = 0xffff0000;
+r.width = 50;
+r.height = 50;
+
+var s = new enesim.surface(enesim.format.ARGB8888, 320, 240);
+//print("s = " + s);
+
+var b = s.getBuffer();
+//print("b = " + b);
+
+r.draw(s, enesim.rop.FILL, null, 0, 0, null);
+enesim.image.file.save("out.png", b,  null, null);
 enesim.shutdown();
